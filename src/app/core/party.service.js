a34e4c8e39106c477cd9a78be0ba54bcd5225f0c
Heather B <heatherbrokmeier@gmail.com>
2016-06-11 14:42:45 -0500

    Add feature for colum with notified time

--- src/app/core/party.service.js
+++ src/app/core/party.service.js
@@ -1,44 +1,45 @@
-(function(){
+(function() {
     'use strict';
-    
+
     angular
         .module('app.core')
         // returns JS object, so you can put your stuff there and do functions on that
         // function that's run when service is created
         .factory('partyService', partyService);
-    
+
     partyService.$inject = ['$firebaseArray', 'firebaseDataService'];
-    
+
     // returns object that is used througout application
     function partyService($firebaseArray, firebaseDataService) {
         var parties = null;
-        
+
         var service = {
             Party: Party,
             getPartiesByUser: getPartiesByUser,
             reset: reset
         };
-        
+
         return service;
-        
+
         //////////////
-        
+
         function Party() {
             this.name = '';
             this.phone = '';
             this.size = '';
             this.done = false; // Has this party been served already?
             this.notified = false; // Has party gotten text message?
+            this.notifiedTime = null; // Time notfication was sent.
+            this.time = Date.now(); // Time party was added.
         }
-        
+
         function getPartiesByUser(uid) {
             if (!parties) {
                 parties = $firebaseArray(firebaseDataService.users.child(uid).child('parties'));
-               
             }
             return parties;
         }
-        
+
         function reset() {
             if (parties) {
                 parties.$destroy();
